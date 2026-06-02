7d8d0463b9cdabc0356e11f65a5b7eda2cffe4b5
luka4723 <lmilic4723rn@raf.rs>
2026-06-02 23:06:04 +0200

    Add files via upload

---
+++ src/mancala.hs
@@ -0,0 +1,95 @@
+import Text.Printf
+import Rose
+
+
+data Small = Small Int Int Int Int Int Int
+data Turn =  Player | Computer deriving (Show, Eq)
+data Mancala = Mancala {turn :: Turn, pBig :: Int, cBig :: Int, pSmall :: Small, cSmall :: Small}
+
+instance Show Small where
+    show (Small a b c d e f) = printf "   %2d %2d %2d %2d %2d %2d\n" a b c d e f
+
+adjust :: Int -> String
+adjust a
+       | a>=10 = show a
+       | otherwise = " " ++ show a 
+
+instance Show Mancala where
+    show (Mancala turn pBig cBig pSmall cSmall) = printf $ show cSmall ++ adjust cBig ++ "                   " ++ show pBig ++ "\n"
+                                                         ++ show pSmall
+
+valid :: Mancala -> [Int]
+valid (Mancala turn pBig cBig pSmall cSmall)
+        | turn == Player = [i | i <- [1..6], let (Small a b c d e f) = pSmall, let x = case i of
+                                                                                      1 -> a
+                                                                                      2 -> b
+                                                                                      3 -> c
+                                                                                      4 -> d
+                                                                                      5 -> e
+                                                                                      6 -> f, x > 0]
+        | turn == Computer = [i | i <- [12,11..7], let (Small a b c d e f) = cSmall, let x = case i of
+                                                                                              12 -> a
+                                                                                              11 -> b
+                                                                                              10 -> c
+                                                                                              9 -> d
+                                                                                              8 -> e
+                                                                                              7 -> f, x > 0]
+
+doMove :: Mancala -> Int -> Mancala
+doMove (Mancala turn pBig cBig pSmall cSmall) n
+        | turn == Player = if n `elem` (valid (Mancala Player pBig cBig pSmall cSmall)) then handle (Mancala turn pBig cBig pSmall cSmall) n else error "Los input"
+        | turn == Computer = if n `elem` valid (Mancala Computer pBig cBig pSmall cSmall) then handle (Mancala turn pBig cBig pSmall cSmall) n else error "Los input"
+        where
+            fromSmall (Small a b c d e f) = [a,b,c,d,e,f]
+            toSmall [a,b,c,d,e,f] = (Small a b c d e f)
+            handle (Mancala Player pBig cBig pSmall cSmall) n = do let  xs = fromSmall pSmall
+                                                                   let  whole = xs ++ [pBig] ++ (reverse . fromSmall $ cSmall)
+                                                                   let  val = xs !! (n-1) `mod` 13
+                                                                   let  val' = xs !! (n-1) `div` 13
+                                                                   let  newPSmall = [x+i| (x,j)<-zip ((take (n-1) whole ++ [0])) [1..], let i = if j<=val-13+n then 1 else 0]
+                                                                                    ++ [x+i| (x,j)<-zip (drop n whole) [1..], let i = if j<=val then 1 else 0]
+                                                                   let newPSmall' = map (+val') newPSmall
+                                                                   let newTurn = if val + n == 7 then Player else Computer
+                                                                   let newPSmall'' = if val' == 0 && val+n<7 && newPSmall' !! (val + n - 1) == 1 then [ i | (x,j)<- zip newPSmall' [1..], let i = if j==7 then 1+x+ (newPSmall' !! (13-n)) else x] 
+                                                                                                                   else newPSmall'
+                                                                   let newPSmall''' = if val' == 0 && val+n<7 && newPSmall'' !! (val + n - 1) == 1 then [ i | (x,j)<- zip newPSmall'' [1..], let i = if j==val+n || j==12-val+n then 0 else x] 
+                                                                                                                   else newPSmall''
+                                                                   (Mancala newTurn (head . take 1 . drop 6 $ newPSmall''') cBig
+                                                                    (toSmall $ take 6 newPSmall''') (toSmall $ take 6 (reverse newPSmall''')))
+                                                                   
+            handle (Mancala Computer pBig cBig pSmall cSmall) n = do let  xs = reverse . fromSmall $ cSmall
+                                                                     let  whole = xs ++ [cBig] ++ fromSmall pSmall
+                                                                     let  val = (xs !! (n-7)) `mod` 13
+                                                                     let  val' = (xs !! (n-7)) `div` 13
+                                                                     let  newCSmall = [x+i| (x,j)<-zip ((take (n-7) whole ++ [0])) [1..], let i = if j<=val-19+n then 1 else 0]
+                                                                                    ++ [x+i| (x,j)<-zip (drop (n-6) whole) [1..], let i = if j<=val then 1 else 0 ]
+                                                                     let newCSmall' = map (+val') newCSmall 
+                                                                     let newTurn = if val + (n-7) == 7 then Computer else Player
+                                                                     let newCSmall'' = if val' == 0 && val+(n-6)<7 && newCSmall' !! (val + (n-6) - 1) == 1 then [ i | (x,j)<- zip newCSmall' [1..], let i = if j==7 then 1+x+ (newCSmall' !! (13-(n-6))) else x] 
+                                                                                                                   else newCSmall'
+                                                                     let newCSmall''' = if val' == 0 && val+(n-6)<7 && newCSmall'' !! (val + (n-6) - 1) == 1 then [ i | (x,j)<- zip newCSmall'' [1..], let i = if j==val+(n-6) || j==12-val+(n-6) then 0 else x] 
+                                                                                                                   else newCSmall''
+                                                                     (Mancala newTurn pBig  (head . take 1 . drop 6 $ newCSmall''')
+                                                                      (toSmall . reverse . take 6 $ reverse newCSmall''') (toSmall . reverse . take 6 $ newCSmall'''))
+
+isGameOver :: Mancala -> Bool
+isGameOver (Mancala turn pBig cBig pSmall cSmall) = if ((==0) . sum . fromSmall $ pSmall) || ((==0) . sum . fromSmall $ cSmall)
+                                                    then True else False
+                                                    where fromSmall (Small a b c d e f) = [a,b,c,d,e,f]
+
+getWinner :: Mancala -> Turn
+getWinner (Mancala turn pBig cBig pSmall cSmall) = if ((==0) . sum . fromSmall $ pSmall)
+                                                    then Player else Computer
+                                                    where fromSmall (Small a b c d e f) = [a,b,c,d,e,f] 
+
+genMoves :: Rose Mancala -> Int -> Rose Mancala
+genMoves (Node (Mancala turn pBig cBig pSmall cSmall) xs) 0 = (Node (Mancala turn pBig cBig pSmall cSmall) xs)
+genMoves (Node (Mancala turn pBig cBig pSmall cSmall) xs) n = genMoves (Node (Mancala turn pBig cBig pSmall cSmall) 
+                                                              [(Node (doMove (Mancala turn pBig cBig pSmall cSmall) i) []) | i<-valid (Mancala turn pBig cBig pSmall cSmall)]) (n-1)
+main = do
+        let mancala = Mancala Computer 0 0 (Small 4 4 4 4 4 4) (Small 4 4 4 4 4 4)
+        --putStrLn $ show mancala
+        let rose = genMoves (Node mancala []) 3
+        putStrLn $ show $ elemsOnDepth rose 1
+
+        
---
+++ src/rose.hs
@@ -0,0 +1,52 @@
+module Rose where
+
+data Rose a = Node a [Rose a]
+
+-- a) size - vraća broj čvorova stabla
+-- height - računa visinu stabla, odnosno najdužu putanju (broj grana) od korena do lista
+size :: Rose a -> Int
+size (Node a xs) = 1 + foldl (\acc x -> acc + size x) 0 xs
+
+height :: Rose a -> Int
+height (Node a []) = 0
+height (Node a xs) = maxn $ map (\x -> 1 + height x) xs
+                     where
+                        maxn = foldl (\acc x-> if acc > x then acc else x) 0 
+
+-- b) leaves - vraća listu koja sadrži vrednosti svih listova stabla
+leaves :: Rose a -> [a]
+leaves (Node a []) = [a]
+leaves (Node a xs) = concatMap leaves xs
+
+-- c) elemsOnDepth - vraća vrednosti svih elemenata na određenoj dubini stabla
+elemsOnDepth :: Rose a -> Int -> [a] -- Maybe??
+elemsOnDepth (Node a xs) 0 = [a]
+elemsOnDepth (Node a xs) n = concatMap (\x -> elemsOnDepth x (n-1)) xs
+
+-- d) instancirati tipsku klasu Functor za tip podataka Rose
+instance Functor Rose where
+    fmap f (Node a xs) = Node (f a) (map (fmap f) xs) 
+
+-- e) napisati funkciju foldRose koja izršava fold (levi ili desni) na svim čvorovima stabla tipa Rose (na primer
+-- ako imamo stablo sa celim brojevima i prosledimo funkciju (+) i akumulator 0 kao rezultat se vraća zbir svih
+-- čvorova)
+foldRose :: (b -> a -> b) -> b -> Rose a -> b
+foldRose f acc (Node a xs) = foldl f acc (elems (Node a xs))
+                             where 
+                                elems (Node a []) = [a]
+                                elems (Node a xs)= a:(concatMap elems xs)
+
+demo = do
+        let tree = Node 1 [Node 2 [], Node 3 []]
+        let tree = Node 1 [Node 2 [ Node 5 [], Node 6 [] ], Node 3 [], Node 4[ Node 7 []]]
+        putStrLn $ show $ size tree
+        putStrLn $ show $ height tree
+        putStrLn $ show $ leaves tree
+        putStrLn $ show $ elemsOnDepth tree 0
+        putStrLn $ show $ elemsOnDepth tree 1
+        putStrLn $ show $ elemsOnDepth tree 2
+        let tree2 = fmap (+1) tree
+        putStrLn $ show $ elemsOnDepth tree2 0
+        putStrLn $ show $ elemsOnDepth tree2 1
+        putStrLn $ show $ elemsOnDepth tree2 2
+        putStrLn $ show $ foldRose (+) 0 tree
